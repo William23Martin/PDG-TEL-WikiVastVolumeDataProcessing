999f6a7209fdc6d5a81489a3a247ed07a2b929c4
William Martín <willy23martin@gmail.com>
2017-01-01 09:58:40 -0500

    Customized Stage Configurator
    Contains the model classes of the test stage. The files that it will can
    be configure will be in Banking Context as: Credit Cards, Saving/Current
    Accounts,  Natural Persons, Legal Persons, and their credit/debit
    movements.

---
+++ Code/CustomizedStageConfigurator/src/pdg/tel/vvd/test/configurator/stage/model/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This is a package that contains the model classes of the
+ * test stage. The stages that it will can be configure will 
+ * be in Banking Context as: Credit Cards, Saving/Current Accounts,
+ * Natural Persons, Legal Persons, and their credit/debit movements.
+ */
+/**
+ * @author William Martín Chávez González.
+ * Systems Engineer and Communications Engineering Student.
+ * Icesi University, Cali, Colombia. 
+ */
+package pdg.tel.vvd.test.configurator.stage.model;
