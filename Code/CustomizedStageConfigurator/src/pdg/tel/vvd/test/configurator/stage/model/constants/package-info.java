b895c31814edbfd09a57d5bdf9a5b3947851aa99
William Martín <willy23martin@gmail.com>
2017-01-02 12:28:14 -0500

    Actualization

---
+++ Code/CustomizedStageConfigurator/src/pdg/tel/vvd/test/configurator/stage/model/constants/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The Test Banking Data initializers.
+ */
+/**
+ * @author William Martín Chávez González.
+ * Systems Engineer and Communications Engineering Student.
+ * Icesi University, Cali, Colombia. 
+ */
+package pdg.tel.vvd.test.configurator.stage.model.constants;
