184f54a1cfa2d9c22a909c41b2d99b3e2a8529d1
William Martín <willy23martin@gmail.com>
2017-01-06 21:06:52 -0500

    Actualization

---
+++ Code/TestXMLFile1019MicroService/src/pdg/tel/vvd/test/usecase/requirement/xml1019file/model/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This is a package that contains the classes of the model of the
+ * 1019 XML file requirement for PDG-TEL use case. 
+ * This packet uses the JDOM 2.0.6 library.
+ */
+/**
+ * @author William Martín Chávez González.
+ * Systems Engineer and Communications Engineering Student.
+ * Icesi University, Cali, Colombia. 
+ */
+package pdg.tel.vvd.test.usecase.requirement.xml1019file.model;
