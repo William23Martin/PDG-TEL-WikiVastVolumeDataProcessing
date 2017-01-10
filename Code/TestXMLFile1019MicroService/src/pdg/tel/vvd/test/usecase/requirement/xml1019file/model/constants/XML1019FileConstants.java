aee049af043dca138b796637dd12b146fcdff79e
William Martín <willy23martin@gmail.com>
2017-01-09 21:22:36 -0500

    Actualization

--- Code/TestXMLFile1019MicroService/src/pdg/tel/vvd/test/usecase/requirement/xml1019file/model/constants/XML1019FileConstants.java
+++ Code/TestXMLFile1019MicroService/src/pdg/tel/vvd/test/usecase/requirement/xml1019file/model/constants/XML1019FileConstants.java
@@ -15,7 +15,7 @@ public enum XML1019FileConstants {
 	public final static String ARCHIVO_NOMBRE_SEPARADOR= "_";//Attachment restriction.
 	public final static String ARCHIVO_NOMBRE_CONCEPTO_INSERCION="01";//Attachment restriction.
 	public final static String ARCHIVO_NOMBRE_CONCEPTO_REEMPLAZO="02";//Attachment restriction.
-	public final static String ARCHIVO_NOMBRE_MOVIMIENTO_CC_CA= "00019"; //Attachment restriction.
+	public final static String ARCHIVO_NOMBRE_FORMATO= "00019"; //Attachment restriction.
 	public final static String ARCHIVO_NOMBRE_VERSION= "09";//Attachment restriction.
 	
 	//Header constants.
