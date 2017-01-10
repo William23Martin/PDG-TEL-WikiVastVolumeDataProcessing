a32fa61c4f888b5750efb1409580127e67293956
William Martín <willy23martin@gmail.com>
2017-01-10 00:15:45 -0500

    Actualization

--- Code/TestXMLFile1019MicroService/src/pdg/tel/vvd/test/usecase/requirement/xml1019file/model/file/structure/XML1019Content.java
+++ Code/TestXMLFile1019MicroService/src/pdg/tel/vvd/test/usecase/requirement/xml1019file/model/file/structure/XML1019Content.java
@@ -22,9 +22,40 @@ public class XML1019Content {
 	 */
 	private String identificationNumber;
 
-	public XML1019Content(int documentType, String identificationNumber) {
+	/**
+	 * In the XML 1019 file content is the verification digit of CC/CA owner.
+	 * Type:int, Length:1. Mandatory:Only if document type equals 31 (NIT).
+	 * Attachment restriction.
+	 */
+	private Integer verificationDigit;
+
+	/**
+	 * In the XML 1019 file content is the first last name of CC/CA owner.
+	 * Type:string, Length:60. Mandatory:Only if the owner is a Natural Person
+	 * (PN). Attachment restriction.
+	 */
+	private String firstLastName;
+
+	/**
+	 * In the XML 1019 file content is the second last name of CC/CA owner.
+	 * Type:string, Length:60. Mandatory:False. Attachment restriction.
+	 */
+	private String secondLastName;
+
+	/**
+	 * In the XML 1019 file content is the first name of CC/CA owner.
+	 * Type:string, Length:60. Mandatory:Only if the owner is a Natural Person
+	 * (PN). Attachment restriction.
+	 */
+	private String firstName;
+
+	public XML1019Content(int documentType, String identificationNumber, int verificationDigit, String firstLastName,
+			String secondLastName) {
 		this.documentType = new Integer(documentType);
 		this.identificationNumber = identificationNumber;
+		this.verificationDigit = new Integer(verificationDigit);
+		this.firstLastName = firstLastName;
+		this.secondLastName = secondLastName;
 		loadInvariants();
 	}
 
@@ -42,6 +73,13 @@ public class XML1019Content {
 		assert (!identificationNumber.contains(".") || !identificationNumber.contains("-")
 				|| !identificationNumber.contains(",")
 				|| !identificationNumber.contains(" ")) : "CC/CA Owner identification number must be exists"; // Attachment
+		// restriction.
+		assert verificationDigit.toString()
+				.length() == 1 : "CC/CA Owner verification digit lenght must be equal one (1)."; // Attachment
+		// restriction.
+		assert firstLastName.length() == 60 : "CC/CA Owner first last name lenght must be equal sixty (60).";// Attachment
+		// restriction.
+		assert secondLastName.length() == 60 : "CC/CA Owner second last name lenght must be equal sixty (60).";// Attachment
 		// restriction.
 	}
 
