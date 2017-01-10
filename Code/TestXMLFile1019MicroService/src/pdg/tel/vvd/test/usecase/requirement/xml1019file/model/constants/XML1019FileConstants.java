ea20b74a52e22a8d38a6bfeefa5fb5d1b9c55a3a
William Martín <willy23martin@gmail.com>
2017-01-09 21:20:24 -0500

    Actualization

---
+++ Code/TestXMLFile1019MicroService/src/pdg/tel/vvd/test/usecase/requirement/xml1019file/model/constants/XML1019FileConstants.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package pdg.tel.vvd.test.usecase.requirement.xml1019file.model.constants;
+
+/**
+ * @author William Martín Chávez González.
+ * Systems Engineer and Communications Engineering Student.
+ * Icesi University, Cali, Colombia. 
+ */
+public enum XML1019FileConstants {
+;
+	//File name constants.
+	public final static String ARCHIVO_NOMBRE_PREFIJO= "Dmuisca";//Attachment restriction.
+	public final static String ARCHIVO_NOMBRE_SEPARADOR= "_";//Attachment restriction.
+	public final static String ARCHIVO_NOMBRE_CONCEPTO_INSERCION="01";//Attachment restriction.
+	public final static String ARCHIVO_NOMBRE_CONCEPTO_REEMPLAZO="02";//Attachment restriction.
+	public final static String ARCHIVO_NOMBRE_MOVIMIENTO_CC_CA= "00019"; //Attachment restriction.
+	public final static String ARCHIVO_NOMBRE_VERSION= "09";//Attachment restriction.
+	
+	//Header constants.
+	public final static String ARCHIVO_ENCABEZADO_RAIZ= "mas";//Attachment restriction.
+	
+	
+	
+	
+}
---
+++ Code/TestXMLFile1019MicroService/src/pdg/tel/vvd/test/usecase/requirement/xml1019file/model/constants/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author William Martín Chávez González.
+ * Systems Engineer and Communications Engineering Student.
+ * Icesi University, Cali, Colombia. 
+ */
+package pdg.tel.vvd.test.usecase.requirement.xml1019file.model.constants;
